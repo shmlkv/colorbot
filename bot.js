d5451b280ce9de7060e88dcfda4fecf205b6871b
shmlkv <splo1t@icloud.com>
2016-06-21 14:13:21 +0300

    rgb to pic, rgb to hex

--- bot.js
+++ bot.js
@@ -2,95 +2,59 @@
 
 var config = require('./config.json'),
     tg = require('telegram-node-bot')(config.token),
-    // gm = require('gm').subClass({ imageMagick: true }),
     fs = require('fs'),
-    req = require('tiny_request')
+    req = require('tiny_request'),
+    hexrgb = require('hexrgb')
+
 
 tg.router.
     when(['/start'], 'StartController').
     otherwise('OtherwiseController')
 
-tg.controller('PingController', ($) => {
-    tg.for('ping', () => {
-        $.sendMessage('pong')
-    })
+tg.controller('StartController', ($) => {
+    $.sendMessage('Hello! Send me color like #3300ff, #30f or rgb(51,0,255)', { parse_mode: 'Markdown' });
+})
+
+tg.controller('OtherwiseController', ($) => {
+    if (isValidHEX($.message.text)) {
+        if ($.message.text.length === 7) {
+            var color = $.message.text.slice(1, 7);
+        } else if ($.message.text.length === 4) {
+            var color = $.message.text[1] + $.message.text[1]
+                + $.message.text[2] + $.message.text[2]
+                + $.message.text[3] + $.message.text[3]
+        } else { errIsntColor() }
+        
+        sendColorPic($, color)
+    } else if (isValidRGB($.message.text)) { // TODO 
+        console.log(hexrgb.rgb2hex($.message.text))
+        sendColorPic($, hexrgb.rgb2hex($.message.text).slice(1))
+    } else {
+        errIsntColor()
+    }
+    function errIsntColor() {
+        $.sendMessage('I dont think that it\'s a color', { parse_mode: 'Markdown' });
+    }
+    function isValidRGB(sample) {
+        var regexp = /^(\brgba?\()( ?[0-9](\d{1,2}) ?), ?( ?[0-9](\d{1,2}) ?), ?( ?[0-9](\d{1,2}) ?)?(,?)?( ?[0-9](\d{0,2}) ?)\)/;
+        return regexp.test(sample);
+    }
+    function isValidHEX(sample) {
+        var regexp = /^#([0-9a-fA-F]{3}|[0-9a-fA-F]{6})$/;
+        return regexp.test(sample);
+    }
+    function sendColorPic($, color) {
+            var filename = '.\\temp\\' + color + '.png'
+            var wstream = fs.createWriteStream(filename)
+
+            wstream.on('finish', () => {
+                $.sendPhoto(fs.createReadStream(filename))
+                fs.unlink(filename)
+            })
+
+            req.get({
+                url: 'http://www.colorhexa.com/' + color + '.png',
+                pipe: wstream
+            })
+        }
 }) 
-
-// tg.controller('OtherwiseController', ($) => {
-//     //  if (true) {
-//     var color = $.message.text.slice(1, 6);
-//     var fileName = Math.random().toString(16) + ".jpg"
-//     var wstream = fs.createWriteStream(__dirname + "/"  + fileName)
-//     var self = this
-//     req.get({
-//         url: 'http://www.colorhexa.com/ffff00.png',
-//         pipe: wstream
-//     })
-//     console.log(__dirname + "/" + fileName)
-//     wstream.on('finish', () => {
-//         $.sendPhoto($.chatId, fs.createReadStream(__dirname + "/" + fileName), (body, err) => {
-//             fs.unlink(__dirname + "/" + fileName)
-//             if (!err) {
-//                 callback(body)
-//             } else {
-//                 callback(undefined, err)
-//             }
-//         })
-//     })
-// })
-    
-    // wstream.on('finish', () => {
-    //     sendPhoto($.chatId, fs.createReadStream(__dirname + '/temp/' + fileName), (body, err) => {
-    //         fs.unlink(__dirname + '/temp/' + fileName)
-    //         if (!err) {
-    //             callback(body)
-    //         } else {
-    //             callback(undefined, err)
-    //         }
-    //     })
-    // })
-    // req.get({
-    //     url: 'http://www.colorhexa.com/ffff00.png',
-    //     pipe: wstream
-    // })
-
-
-// //sendPhotoFromUrl(chatId, url, options, callback) {
-//     callback = callback || Function()
-//     var fileName = Math.random().toString(16) + ".jpg"
-//     var wstream = fs.createWriteStream(__dirname + '/temp/' + fileName)
-//     var self = this
-
-
-//     wstream.on('finish', () => {
-//         self.sendPhoto(chatId, fs.createReadStream(__dirname + '/temp/' + fileName), options, (body, err) => {
-//             fs.unlink(__dirname + '/temp/' + fileName)
-//             if (!err) {
-//                 callback(body)
-//             } else {
-//                 callback(undefined, err)
-//             }
-//         })
-//     })
-
-//     req.get({
-//         url: url,
-//         pipe: wstream
-//     })
-// //})
-
-
-    // }
-    // var x = gm(200, 400, "#ddff99f3")
-    //     .drawText(10, 50, "from scratch")
-    //     .write("brandNewImg.jpg", function (err) {
-    //         console.log(err)
-    //     });
-
-
-    //     var doc = {
-    //         value: x, //stream
-    //         filename: 'photo.png',
-    //         contentType: 'image/png'
-    //     }
-    //     $.sendDocument(doc)
--- web.js
+++ web.js
@@ -7,7 +7,7 @@ app.get('/', function (req, res) {
   res.json({ version: packageInfo.version });
 });
 
-var server = app.listen(3030, function () { // process.env.PORT
+var server = app.listen(8081, function () { // process.env.PORT
   var host = server.address().address;
   var port = server.address().port;
 
