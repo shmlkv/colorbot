77ce8b15f7c119e1bb318be7b44610288cf8a271
shmlkv <splo1t@icloud.com>
2016-06-21 20:36:13 +0300

    /tohex + /torgba

--- bot.js
+++ bot.js
@@ -4,17 +4,39 @@ var config = require('./config.json'),
     tg = require('telegram-node-bot')(config.token),
     fs = require('fs'),
     req = require('tiny_request'),
-    hexrgb = require('hexrgb')
-
+    hexrgb = require('hexrgb'),
+    ColorThief = require("thief");
+    colorThief = new ColorThief()
 
 tg.router.
     when(['/start'], 'StartController').
+    when(['/toHEX :color'], 'ToHexController').
+    when(['/toRGB :color'], 'ToRgbController').
     otherwise('OtherwiseController')
 
 tg.controller('StartController', ($) => {
-    $.sendMessage('Hello! Send me color like #3300ff, #30f or rgb(51,0,255)', { parse_mode: 'Markdown' });
+    $.sendMessage('Hello! 👋\n\nWhat I can do:\n\n• Preview of colors like: `#3300ff`, `#30f` or `rgb(51,0,255)`\n• Convert RGB to HEX /toHEX `rgb(51,0,255)` \n• Convert HEX to RGB /toRGB `#3300ff`', { parse_mode: 'Markdown' });
 })
-
+tg.controller('ToHexController', ($) => {
+    tg.for('/toHEX :color', ($) => {
+        console.log($.query.color)
+        if (isValidRGB($.query.color)) {
+            $.sendMessage(hexrgb.rgb2hex($.query.color))
+        } else {
+            $.sendMessage('It\'s not valid')
+        }
+    })
+})
+tg.controller('ToRgbController', ($) => {
+    tg.for('/toRGB :color', ($) => {
+        console.log($.query.color)
+        if (isValidHEX($.query.color)) {
+            $.sendMessage(hexrgb.hex2rgb($.query.color))
+        } else {
+            $.sendMessage('It\'s not valid')
+        }
+    })
+})
 tg.controller('OtherwiseController', ($) => {
     if (isValidHEX($.message.text)) {
         if ($.message.text.length === 7) {
@@ -23,38 +45,43 @@ tg.controller('OtherwiseController', ($) => {
             var color = $.message.text[1] + $.message.text[1]
                 + $.message.text[2] + $.message.text[2]
                 + $.message.text[3] + $.message.text[3]
-        } else { errIsntColor() }
-        
+        } else { errIsntColor($) }
+
         sendColorPic($, color)
     } else if (isValidRGB($.message.text)) { // TODO 
         console.log(hexrgb.rgb2hex($.message.text))
         sendColorPic($, hexrgb.rgb2hex($.message.text).slice(1))
     } else {
-        errIsntColor()
+        errIsntColor($)
     }
-    function errIsntColor() {
-        $.sendMessage('I dont think that it\'s a color', { parse_mode: 'Markdown' });
-    }
-    function isValidRGB(sample) {
-        var regexp = /^(\brgba?\()( ?[0-9](\d{1,2}) ?), ?( ?[0-9](\d{1,2}) ?), ?( ?[0-9](\d{1,2}) ?)?(,?)?( ?[0-9](\d{0,2}) ?)\)/;
-        return regexp.test(sample);
-    }
-    function isValidHEX(sample) {
-        var regexp = /^#([0-9a-fA-F]{3}|[0-9a-fA-F]{6})$/;
-        return regexp.test(sample);
-    }
-    function sendColorPic($, color) {
-            var filename = '.\\temp\\' + color + '.png'
-            var wstream = fs.createWriteStream(filename)
 
-            wstream.on('finish', () => {
-                $.sendPhoto(fs.createReadStream(filename))
-                fs.unlink(filename)
-            })
+})
+function sendColorPic($, color) {
+    var filename = '.\\temp\\' + color + '.png'
+    var wstream = fs.createWriteStream(filename)
 
-            req.get({
-                url: 'http://www.colorhexa.com/' + color + '.png',
-                pipe: wstream
-            })
-        }
-}) 
+    wstream.on('finish', () => {
+        $.sendPhoto(fs.createReadStream(filename))
+        var image = fs.readFileSync(filename);
+        var rgb = colorThief.getColor(image);
+        console.log(rgb)
+        fs.unlink(filename)
+    })
+
+    req.get({
+        url: 'http://www.colorhexa.com/' + color + '.png',
+        pipe: wstream
+    })
+}
+function errIsntColor($) {
+    $.sendMessage('I dont think that it\'s a color', { parse_mode: 'Markdown' });
+}
+function isValidRGB(sample) {
+    var regexp = /^(\brgba?\()( ?[0-9](\d{0,2}) ?), ?( ?[0-9](\d{0,2}) ?), ?( ?[0-9](\d{0,2}) ?)?(,?)?( ?[0-9](\d{0,2}) ?)\)/;
+    return regexp.test(sample);
+}
+function isValidHEX(sample) {
+    var regexp = /^#([0-9a-fA-F]{3}|[0-9a-fA-F]{6})$/;
+    console.log(regexp.test(sample))
+    return regexp.test(sample);
+} 
